89a1b35d38572026747b5651c0e07f69037da9f2
Francesco Rubino <rubinof@gmail.com>
2015-01-19 23:13:46 +0100

    rimosso la parte di script in azienda.hbs (adesso è in azienda-view.js)

--- src/main/webapp/js/view/azienda-view.js
+++ src/main/webapp/js/view/azienda-view.js
@@ -10,7 +10,16 @@ function (Backbone, Resthub, JssorSlider, aziendaTemplate) {
 
             // Render the view
             this.render();
-        }
+        },
+        
+        render: function() {
+        	AziendaView.__super__.render.apply(this, arguments);
+        	var options = {
+        	         $DragOrientation: 3,
+        	         //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+        	     };
+        	var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+          },
 
     });
     return AziendaView;
