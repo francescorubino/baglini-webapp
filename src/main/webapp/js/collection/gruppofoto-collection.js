436e011d8268f7fff0e5bc4646f9262f5f022e69
Marco Chiappini <burundini@gmail.com>
2015-01-28 21:19:32 +0100

    Modificati gruppofoto-collection e gruppoFoto-view per permettere la paginazione

--- src/main/webapp/js/collection/gruppofoto-collection.js
+++ src/main/webapp/js/collection/gruppofoto-collection.js
@@ -1,20 +1,50 @@
-define(['backbone', 'model/gruppofoto'], function (Backbone, GruppoFotoModel) {
+define(['backbone-paginator', 'model/gruppofoto'], function (Paginator, GruppoFotoModel) {
 
-    var GruppoFotoCollection = Backbone.Collection.extend({
+    var GruppoFotoCollection = Backbone.Paginator.clientPager.extend({
 
         // Reference to this collection's model.
         model: GruppoFotoModel,
         url:'gruppofoto/list',
+        paginator_core:{
+            // the type of the request (GET by default)
+            type:'GET',
+
+            // the type of reply (json by default)
+            dataType:'json',
+
+            // the URL (or base URL) for the service
+            url:'gruppofoto/list',
+        },
+        paginator_ui:{
+            // the lowest page index your API allows to be accessed
+            firstPage:1,
+
+            // which page should the paginator start from
+            // (also, the actual page the paginator is on)
+            currentPage:1,
+
+            // how many items per page should be shown
+            perPage:1,
+
+            // a default number of total pages to query in case the API or
+            // service you are using does not support providing the total
+            // number of pages for us.
+            // 10 as a default in case your service doesn't return the total
+            totalPages:10
+        },
+        parse:function (response) {
+            return response;
+        },
         
         initialize: function(args){
         	if(typeof args.nomeSezione != 'undefined'){
-        		this.url = 'gruppofoto/gruppoBySezione?nomeSezione=' + args.nomeSezione;
+        		this.paginator_core.url = 'gruppofoto/gruppoBySezione?nomeSezione=' + args.nomeSezione;
         	}
         	if(typeof args.idGruppo != 'undefined'){
-        		this.url = 'gruppofoto/findByGroup?idGruppo=' + args.idGruppo;
+        		this.paginator_core.url = 'gruppofoto/findByGroup?idGruppo=' + args.idGruppo;
         	}
         	if(typeof args.nome != 'undefined'){
-        		this.url = 'gruppofoto/gruppoByNome?nome=' + args.nome;
+        		this.paginator_core.url = 'gruppofoto/gruppoByNome?nome=' + args.nome;
         	}
         },
 
--- src/main/webapp/js/view/gruppoFoto-view.js
+++ src/main/webapp/js/view/gruppoFoto-view.js
@@ -1,24 +1,39 @@
-define([ 'backbone', 'resthub', 'collection/gruppofoto-collection', 'hbs!template/gruppoFoto' ],
-function (Backbone, Resthub, GruppoFotoCollection, gruppoFotoTemplate) {
-    
-    var GruppoFotoView = Resthub.View.extend({
-        
-        // Define view template
-        template: gruppoFotoTemplate,
+define([ 'backbone', 'resthub', 'collection/gruppofoto-collection',
+		'hbs!template/gruppoFoto' ], function(Backbone, Resthub,
+		GruppoFotoCollection, gruppoFotoTemplate) {
 
-        initialize:function (args) {
-            // Initialize the collection
-            this.collection = new GruppoFotoCollection({
-            	nomeSezione : args.nomeSezione
-            });
-            
-            // Render the view when the collection is retreived from the server
-            this.listenTo(this.collection, 'sync', this.render);
-            
-            // Request unpaginated URL
-            this.collection.fetch({ data: { page: 'no'} });
-        }
+	var GruppoFotoView = Resthub.View.extend({
 
-    });
-    return GruppoFotoView;
+		// Define view template
+		template : gruppoFotoTemplate,
+
+		events : {
+		// events to change page
+		},
+
+		initialize : function(args) {
+			// Initialize the collection
+			this.collection = new GruppoFotoCollection({
+				nomeSezione : args.nomeSezione
+			});
+
+			// Render the view when the collection is retrieved from the server
+			this.listenTo(this.collection, 'sync', this.render);
+			// Render the view when the page is changed
+			this.listenTo(this.collection, 'reset', this.render);
+
+			// Request first page
+			this.collection.fetch({
+				success : function() {
+					this.collection.goTo(1);
+				}.bind(this),
+				error : function(collection, response) {
+					console.log(collection);
+					console.log(response);
+				}
+			});
+		}
+
+	});
+	return GruppoFotoView;
 });
